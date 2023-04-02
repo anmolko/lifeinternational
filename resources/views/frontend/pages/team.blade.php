d3edf2b49bd5efef2a44dfa862bdb1342301bdbe
anmolko <anmol@canosoft.com.np>
2023-04-02 10:34:32 +0545

    team redesign update

--- resources/views/frontend/pages/team.blade.php
+++ resources/views/frontend/pages/team.blade.php
@@ -25,52 +25,43 @@
         </div>
     </div>
 
-    <div class="team__area section-padding dark__image">
+    <div class="team__two section-padding">
         <div class="container">
             @if(count($teams) > 0)
-                <div class="row">
-                    @foreach($teams as $team)
-                        <div class="col-xl-3 col-lg-4 col-md-6 mb-30">
-                        <div class="team__area-item">
-                            <div class="team__area-item-image">
-                                <img src="{{ ($team->image!==null) ? asset('/images/teams/'.$team->image): asset('assets/backend/images/users/user-dummy-img.jpg')}}">
-                                <div class="team__area-item-image-icon page">
-                                    <div class="team__area-item-image-social">
-                                        <ul>
-                                            @if(!empty(@$team->fb))
-                                                <li><a href="{{$team->fb}}"><i class="fab fa-facebook-f"></i></a></li>
-                                            @endif
-                                            @if(!empty(@$team->twitter))
-                                                <li><a href="{{$team->twitter}}"><i class="fab fa-twitter"></i></a></li>
-                                            @endif
-                                            @if(!empty(@$team->linkedin))
-                                                <li><a href="{{$team->twitter}}"><i class="fab fa-linkedin"></i></a></li>
-                                            @endif
-                                            @if(!empty(@$team->insta))
-                                                <li><a href="{{$team->twitter}}"><i class="fab fa-instagram"></i></a></li>
-                                            @endif
-                                        </ul>
-                                    </div>
-                                    <span><i class="fas fa-share-alt"></i></span>
-                                </div>
-                            </div>
-                            <div class="team__area-item-content page">
-                                <h5><a>{{ucfirst(@$team->name)}}</a></h5>
-                                <span class="text-eight">{{ucfirst(@$team->post)}}</span>
+            <div class="row">
+                @foreach($teams as $team)
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="team__two-item">
+                        <div class="team__two-item-image">
+                            <img src="{{ ($team->image!==null) ? asset('/images/teams/'.$team->image): asset('assets/backend/images/users/user-dummy-img.jpg')}}" alt="">
+                        </div>
+                        <div class="team__two-item-content">
+                            <h4><a href="#">{{ucfirst(@$team->name)}}</a></h4>
+                            <span class="text-eight">{{ucfirst(@$team->post)}}</span>
+                            <div class="team__two-item-content-social">
+                                <ul>
+                                    @if(!empty(@$team->fb))
+                                        <li><a href="{{$team->fb}}"><i class="fab fa-facebook-f"></i></a></li>
+                                    @endif
+                                    @if(!empty(@$team->twitter))
+                                        <li><a href="{{$team->twitter}}"><i class="fab fa-twitter"></i></a></li>
+                                    @endif
+                                    @if(!empty(@$team->linkedin))
+                                        <li><a href="{{$team->twitter}}"><i class="fab fa-linkedin"></i></a></li>
+                                    @endif
+                                    @if(!empty(@$team->insta))
+                                        <li><a href="{{$team->twitter}}"><i class="fab fa-instagram"></i></a></li>
+                                    @endif
+                                </ul>
                             </div>
                         </div>
                     </div>
-                    @endforeach
                 </div>
-                <div class="row mt-70">
-                    <div class="col-xl-12 t-center">
-                        <h6>Consulting With our Expert Team Members <a href="{{route('contact')}}">schedule meeting</a></h6>
-                    </div>
-                </div>
+                @endforeach
+            </div>
             @endif
         </div>
     </div>
-
 @endsection
 @section('js')
 @endsection
